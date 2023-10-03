f0de2fc24afd61938ebe36cc00abaf6db4e32da5
krasimir-manolov <145689991+krasimir-manolov@users.noreply.github.com>
2023-10-03 13:28:18 +0300

    Updat_2_guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -13,7 +13,7 @@ while True:
     player_number = int(player_input)
     if player_number == computer_number:
         print('You guess it!')
-        if counter < 3:
+        if counter <= 3:
             print('Excellent!!!')
         else:
             print('Good job!')
