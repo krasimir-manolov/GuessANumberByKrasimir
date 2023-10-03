089fd7f0d3b5b189b09a6682c366e1e164e8dedb
krasimir-manolov <demokras1@abv.bg>
2023-10-03 13:22:31 +0300

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -13,6 +13,10 @@ while True:
     player_number = int(player_input)
     if player_number == computer_number:
         print('You guess it!')
+        if counter < 3:
+            print('Excellent!!!')
+        else:
+            print('Good job!')
         print(f'Number of attempts: {counter}')
         break
     elif player_number > computer_number:
